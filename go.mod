0eaf87cb9145a120848ac2294ef53ed2c3134766
Mikkel Juul <mju@dmi.dk>
2021-02-15 21:28:25 +0100

    Benchmarks all around!
    added goProto, gogoProto benchmark

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/MikkelHJuul/grpcConst
 go 1.15
 
 require (
-	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	github.com/gogo/protobuf v1.3.2
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
 )
--- interceptor_benchmark_test.go
+++ interceptor_benchmark_test.go
@@ -4,8 +4,11 @@ import (
 	ogcIsh "github.com/MikkelHJuul/grpcConst/examples/ogc_ish/proto"
 	"github.com/MikkelHJuul/grpcConst/examples/route_guide/proto"
 	"github.com/MikkelHJuul/grpcConst/merge"
+	gogoProto "github.com/gogo/protobuf/proto"
+	pb "github.com/gogo/protobuf/proto/test_proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
+	goProto "google.golang.org/protobuf/proto"
 	"testing"
 )
 
@@ -115,7 +118,63 @@ func BenchmarkPreCompiled(b *testing.B) {
 				},
 			},
 		}
-		f.DoMerge(
-			&ogcIsh.Feature{Properties: &ogcIsh.Properties{Measurement: &ogcIsh.Measurement{Value: 666}}})
+		r := &ogcIsh.Feature{Properties: &ogcIsh.Properties{Measurement: &ogcIsh.Measurement{Value: 666}}}
+		r.MergeFieldsFrom(*f)
+	}
+}
+
+func BenchmarkProtoMerge(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		goProto.Merge(&ogcIsh.Feature{Properties: &ogcIsh.Properties{Measurement: &ogcIsh.Measurement{Value: 666}}}, &ogcIsh.Feature{
+			Type: "Feature",
+			Properties: &ogcIsh.Properties{
+				Measurement: &ogcIsh.Measurement{
+					Name: "John",
+				},
+				Station: &ogcIsh.Station{
+					Name:     "Some Station Name",
+					Metadata: "Some station's metadata, a short story",
+				},
+			},
+			Geometry: &ogcIsh.Geometry{
+				Type: "Lol",
+				Coordinates: &ogcIsh.Point{
+					Latitude:  123,
+					Longitude: 321,
+				},
+			},
+		})
+	}
+}
+
+func BenchmarkGoGoProtoMerge(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		gogoProto.Merge(&pb.MyMessage{
+			Inner: &pb.InnerMessage{
+				Host: gogoProto.String("niles"),
+				Port: gogoProto.Int32(9099),
+			},
+			Pet: []string{"bunny", "kitty"},
+			Others: []*pb.OtherMessage{
+				{
+					Key: gogoProto.Int64(31415926535),
+				},
+				{
+					// Explicitly test a src=nil field
+					Inner: nil,
+				},
+			},
+		}, &pb.MyMessage{
+			Inner: &pb.InnerMessage{
+				Host:      gogoProto.String("hey"),
+				Connected: gogoProto.Bool(true),
+			},
+			Pet: []string{"horsey"},
+			Others: []*pb.OtherMessage{
+				{
+					Value: []byte("some bytes"),
+				},
+			},
+		})
 	}
 }
